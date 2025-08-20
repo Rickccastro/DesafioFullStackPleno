896f37aeea0c77d2ca38316697cf7fdce9172cc5
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-08-20 10:27:30 -0300

    feat: adição de controllers para as entidades e inversões de dependencia no program.

---
+++ DesafioFullStackPlenoBack/src/Desafio.Api/Controllers/TarefasController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Desafio.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class TarefasController : ControllerBase
+{
+}
---
+++ DesafioFullStackPlenoBack/src/Desafio.Api/Controllers/UsuarioController.cs
@@ -0,0 +1,18 @@
+﻿using Desafio.Application.UseCase.Usuario.Cria;
+using Desafio.Communication.Requests.Usuario;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Desafio.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class UsuarioController : ControllerBase
+{
+    [Route("cria-usuario")]
+    [HttpPost]
+    public async Task<ActionResult> Create([FromServices] ICriaUsuarioUseCase useCase, [FromBody] UsuarioRequest request)
+    {
+        var resultCreateCheckoutUseCase = await useCase.CreateUser(request);
+
+        return Ok(resultCreateCheckoutUseCase);
+    }
+}
