27e7a9dbe1c3a25bfa376e10fc4e545bed407861
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-08-20 10:26:52 -0300

    feat: automapper para mapeamento de requests para entidades

---
+++ DesafioFullStackPlenoBack/src/Desafio.Application/AutoMapper/AutoMapping.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Desafio.Communication.Requests.Usuario;
+using Desafio.Domain.Entities;
+
+namespace Desafio.Application.AutoMapper;
+public class AutoMapping : Profile
+{
+    public AutoMapping()
+    {
+        RequestParaEntidade();
+        EntidadeParaResponse();
+    }
+    private void RequestParaEntidade()
+    {
+        CreateMap<UsuarioRequest, Usuario>()
+        .ForMember(dest => dest.Id, opt => opt.Ignore());
+    }
+    private void EntidadeParaResponse()
+    {
+   
+    }
+}
