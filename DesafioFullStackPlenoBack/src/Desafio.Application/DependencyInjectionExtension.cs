1b64df516b521b9e4913992bf8f88bf452f6be2e
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-08-20 10:26:13 -0300

    feat: adicionei inversão de dependencia e csproj

---
+++ DesafioFullStackPlenoBack/src/Desafio.Application/DependencyInjectionExtension.cs
@@ -0,0 +1,16 @@
+﻿using Desafio.Application.AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Desafio.Application;
+public static class DependencyInjectionExtension
+{
+    public static void AddApplication(this IServiceCollection services)
+    {
+        AddUseCase(services);
+    }
+
+    public static void AddUseCase(IServiceCollection services)
+    {
+    }
+}
