640ca19b088589bc8991bd3278ed57dd49473f8b
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-08-20 23:07:17 -0300

    feat: criação dos endpoints dos casos de uso da Tarefa

--- DesafioFullStackPlenoBack/src/Desafio.Api/Controllers/TarefasController.cs
+++ DesafioFullStackPlenoBack/src/Desafio.Api/Controllers/TarefasController.cs
@@ -1,9 +1,36 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Desafio.Application.UseCase.Tarefa.Atualizar;
+using Desafio.Application.UseCase.Tarefa.Criar;
+using Desafio.Application.UseCase.Tarefa.Deletar;
+using Desafio.Communication.Requests.Tarefa;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Desafio.Api.Controllers;
-[Route("api/[controller]")]
+[Route("[controller]")]
 [ApiController]
 public class TarefasController : ControllerBase
 {
+    [Route("criar-tarefa")]
+    [HttpPost]
+    public async Task<ActionResult> CriarTarefa([FromServices] ICriarTarefaUseCase useCase, [FromBody] CriarTarefaRequest request)
+    {
+        var nomeUsuario = await useCase.CriarTarefa(request);
+
+        return Ok(nomeUsuario);
+    }
+    [Route("atualizar-tarefa")]
+    [HttpPut]
+    public async Task<ActionResult> AtualizarTarefa([FromServices] IAtualizarTarefaUseCase useCase, [FromBody] AtualizarTarefaRequest request)
+    {
+        var nomeUsuario = await useCase.AtualizarTarefa(request);
+
+        return Ok(nomeUsuario);
+    }
+    [Route("deletar-tarefa")]
+    [HttpPut]
+    public async Task<ActionResult> AtualizarTarefa([FromServices] IDeletarTarefaUseCase useCase, [FromBody] DeletarTarefaRequest request)
+    {
+        var nomeUsuario = await useCase.DeletarTarefa(request);
+
+        return Ok(nomeUsuario);
+    }
 }
