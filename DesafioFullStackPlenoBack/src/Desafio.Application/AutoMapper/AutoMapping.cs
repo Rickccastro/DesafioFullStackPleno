44d1d7b2813962890a6802e9ecd955dd7ec23426
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-08-20 22:53:03 -0300

    feat: adicionando classe de configuração dos mapeamentos do AutoMapper

--- DesafioFullStackPlenoBack/src/Desafio.Application/AutoMapper/AutoMapping.cs
+++ DesafioFullStackPlenoBack/src/Desafio.Application/AutoMapper/AutoMapping.cs
@@ -1,6 +1,12 @@
 ﻿using AutoMapper;
+using Desafio.Communication.Requests.Login;
+using Desafio.Communication.Requests.Tarefa;
 using Desafio.Communication.Requests.Usuario;
+using Desafio.Communication.Responses.Login;
+using Desafio.Communication.Responses.Tarefa;
+using Desafio.Communication.Responses.Usuario;
 using Desafio.Domain.Entities;
+using Desafio.Domain.Enums;
 
 namespace Desafio.Application.AutoMapper;
 public class AutoMapping : Profile
@@ -12,11 +18,18 @@ public class AutoMapping : Profile
     }
     private void RequestParaEntidade()
     {
-        CreateMap<UsuarioRequest, Usuario>()
-        .ForMember(dest => dest.Id, opt => opt.Ignore());
+        CreateMap<CriarUsuarioRequest, Usuario>()
+        .ForMember(dest => dest.Id, opt => opt.Ignore())
+        .ForMember(dest => dest.Perfil, opt => opt.MapFrom(src => Enum.Parse<Perfis>(src.Perfil, true)));
+
+        CreateMap<AtualizarUsuarioRequest, Usuario>();
+        CreateMap<AtualizarTarefaRequest, Tarefa>();
     }
     private void EntidadeParaResponse()
     {
-   
+        CreateMap<Usuario, UsuarioResponse>();
+        CreateMap<Tarefa, TarefaResponse>();
+        CreateMap<Usuario, LoginUsuarioResponse>()
+.ForMember(dest => dest.Token, opt => opt.Ignore());
     }
 }
