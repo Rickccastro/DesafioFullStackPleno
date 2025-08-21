aa16298feb1bc660921d3eb43cf894e68892f41f
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-08-20 22:53:33 -0300

    feat: adição do caso de uso "login"

---
+++ DesafioFullStackPlenoBack/src/Desafio.Application/UseCase/Login/ILoginUsuarioUseCase.cs
@@ -0,0 +1,13 @@
+﻿using Desafio.Communication.Requests.Login;
+using Desafio.Communication.Responses.Login;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio.Application.UseCase.Login;
+public interface ILoginUsuarioUseCase
+{
+    Task<LoginUsuarioResponse> LoginUsuario(LoginUsuarioRequest request);
+}
---
+++ DesafioFullStackPlenoBack/src/Desafio.Application/UseCase/Login/LoginUsuarioUseCase.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Desafio.Application.Security;
+using Desafio.Communication.Requests.Login;
+using Desafio.Communication.Responses.Login;
+using Desafio.Communication.Responses.Usuario;
+using Desafio.Domain.Repositories;
+using Desafio.Domain.Repositories.Especificas;
+
+namespace Desafio.Application.UseCase.Login;
+public class LoginUsuarioUseCase : ILoginUsuarioUseCase
+{
+    private readonly IUsuarioRepository _usuarioRepository;
+    private readonly IMapper _mapper;
+    private readonly IAccessTokenGenerator _accessTokenGenerator;
+
+    public LoginUsuarioUseCase(IUsuarioRepository usuarioRepository, IAccessTokenGenerator accessTokenGenerator, IMapper mapper)
+    {
+        _usuarioRepository = usuarioRepository;
+        _mapper = mapper;
+        _accessTokenGenerator = accessTokenGenerator;
+    }
+
+    public async Task<LoginUsuarioResponse> LoginUsuario(LoginUsuarioRequest request)
+    {
+        var usuario = await _usuarioRepository.ObterPorPropriedadeAsync(u=> u.Email == request.Email && u.Senha == request.Senha);
+
+        if (usuario == null)
+            throw new Exception("Login incorreto, verifique seus dados.");
+
+        var loginResponse = _mapper.Map<LoginUsuarioResponse>(usuario);
+
+        loginResponse.Token = _accessTokenGenerator.Generate(usuario);
+
+        return loginResponse;
+    }
+}
