da113980944d7bb595fc6266f01e38e608000a2b
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-08-20 23:01:07 -0300

    feat: criando requests para os casos de uso de criar e atualizar usuarios

---
+++ DesafioFullStackPlenoBack/src/Desafio.Communication/Desafio.Communication/Requests/Usuario/AtualizarUsuarioRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Desafio.Communication.Requests.Usuario;
+public class AtualizarUsuarioRequest
+{
+    public Guid Id { get; set; }
+    public string Nome { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Senha { get; set; } = null!;
+    public string Perfil { get; set; } = null!;
+}
---
+++ DesafioFullStackPlenoBack/src/Desafio.Communication/Desafio.Communication/Requests/Usuario/CriarUsuarioRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Desafio.Communication.Requests.Usuario;
+
+public class CriarUsuarioRequest
+{
+    public string Nome { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Senha { get; set; } = null!;
+    public string Perfil { get; set; } = null!;
+}
