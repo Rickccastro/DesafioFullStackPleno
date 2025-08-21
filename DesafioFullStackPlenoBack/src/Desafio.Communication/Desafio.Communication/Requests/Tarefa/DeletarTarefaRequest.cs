e82bac61243761124ee3139a052854c5ced1671a
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-08-21 20:45:09 -0300

    refactor: retirando propriedades desnecessarias do request do delete

--- DesafioFullStackPlenoBack/src/Desafio.Communication/Desafio.Communication/Requests/Tarefa/DeletarTarefaRequest.cs
+++ DesafioFullStackPlenoBack/src/Desafio.Communication/Desafio.Communication/Requests/Tarefa/DeletarTarefaRequest.cs
@@ -3,8 +3,4 @@ public class DeletarTarefaRequest
 {
     public Guid Id { get; set; }
     public string Titulo { get; set; } = null!;
-    public string Descricao { get; set; } = null!;
-    public DateTime DataCriacao { get; set; }
-    public string Status { get; set; } = null!;
-    public Guid UsuarioId { get; set; }
 }
