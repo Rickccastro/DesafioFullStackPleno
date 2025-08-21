cc3c30ab2ed2e925b7302408acb6bbeab4f71c24
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-08-20 23:07:54 -0300

    feat: adicionando endpoint para atualização do Usuario

--- DesafioFullStackPlenoBack/src/Desafio.Api/Controllers/UsuarioController.cs
+++ DesafioFullStackPlenoBack/src/Desafio.Api/Controllers/UsuarioController.cs
@@ -1,18 +1,31 @@
-﻿using Desafio.Application.UseCase.Usuario.Cria;
+﻿using Desafio.Application.UseCase.Usuario.Atualizar;
+using Desafio.Application.UseCase.Usuario.Cria;
 using Desafio.Communication.Requests.Usuario;
+using Desafio.Domain.Enums;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Desafio.Api.Controllers;
-[Route("api/[controller]")]
+[Route("[controller]")]
+[Authorize(Roles = nameof(Perfis.Administrador))]
 [ApiController]
 public class UsuarioController : ControllerBase
 {
-    [Route("cria-usuario")]
+    [Route("criar-usuario")]
     [HttpPost]
-    public async Task<ActionResult> Create([FromServices] ICriaUsuarioUseCase useCase, [FromBody] UsuarioRequest request)
+    public async Task<ActionResult> CriarUsuario([FromServices] ICriarUsuarioUseCase useCase, [FromBody] CriarUsuarioRequest request)
     {
-        var resultCreateCheckoutUseCase = await useCase.CreateUser(request);
+        var nomeUsuario = await useCase.CriarUsuario(request);
 
-        return Ok(resultCreateCheckoutUseCase);
+        return Ok(nomeUsuario);
+    }
+    [Route("atualizar-usuario")]
+    [Authorize]
+    [HttpPut]
+    public async Task<ActionResult> AtualizarUsuario([FromServices] IAtualizarUsuarioUseCase useCase, [FromBody] AtualizarUsuarioRequest request)
+    {
+        var nomeUsuario = await useCase.AtualizarUsuario(request);
+
+        return Ok(nomeUsuario);
     }
 }
