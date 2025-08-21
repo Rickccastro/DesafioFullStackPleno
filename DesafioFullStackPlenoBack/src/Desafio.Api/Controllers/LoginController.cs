4873cf8fc768e215daa6f9f8943d74ab62668fe0
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-08-20 23:08:18 -0300

    feat: endpoint para o Login do Usuario

---
+++ DesafioFullStackPlenoBack/src/Desafio.Api/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using Desafio.Application.UseCase.Login;
+using Desafio.Communication.Requests.Login;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Desafio.Api.Controllers;
+[Route("[controller]")]
+[ApiController]
+public class LoginController : ControllerBase
+{
+    [Route("login-usuario")]
+    [HttpPost]
+    public async Task<IActionResult> LoginUsuario([FromServices] ILoginUsuarioUseCase useCase, [FromBody] LoginUsuarioRequest request)
+    {
+        var result = await useCase.LoginUsuario(request);
+
+        return Ok(result);
+    }
+}
