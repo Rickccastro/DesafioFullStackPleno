48fbc8fc9a55b9d09f43948dd23cd50d180cdf61
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-08-20 22:56:28 -0300

    feat: criação da classe para gerar o token JWT

---
+++ DesafioFullStackPlenoBack/src/Desafio.Infrastructure/Services/Security/JwtTokenGenerator.cs
@@ -0,0 +1,49 @@
+﻿using Desafio.Application.Security;
+using Desafio.Domain.Entities;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Desafio.Infrastructure.Services.Security;
+internal class JwtTokenGenerator : IAccessTokenGenerator
+{
+    private readonly uint _expirationTimeMinutes;
+    private readonly string _signingKey;
+
+    public JwtTokenGenerator(uint expirationTimeMinutes, string signingKey)
+    {
+        _expirationTimeMinutes = expirationTimeMinutes;
+        _signingKey = signingKey;
+    }
+
+    public string Generate(Usuario usuario)
+    {
+        var claims = new List<Claim>()
+        {
+            new Claim(ClaimTypes.Name, usuario.Nome),
+            new Claim(ClaimTypes.Sid, usuario.Id.ToString()),
+            new Claim(ClaimTypes.Role, usuario.Perfil.ToString()),
+        };
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Expires = DateTime.UtcNow.AddMinutes(_expirationTimeMinutes),
+            SigningCredentials = new SigningCredentials(SecurityKey(), SecurityAlgorithms.HmacSha256Signature),
+            Subject = new ClaimsIdentity(claims)
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+
+        var securityToken = tokenHandler.CreateToken(tokenDescriptor);
+
+        return tokenHandler.WriteToken(securityToken);
+    }
+
+    private SymmetricSecurityKey SecurityKey()
+    {
+        var key = Encoding.UTF8.GetBytes(_signingKey);
+
+        return new SymmetricSecurityKey(key);
+    }
+}
