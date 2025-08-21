b6572a49924f9afba936dc23e2484a643fe2dd7c
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-08-21 20:51:28 -0300

    feat: inclusão de projeto para tratamento de erros

---
+++ DesafioFullStackPlenoBack/src/Desafio.Exception/ExceptionBase/DesafioException.cs
@@ -0,0 +1,12 @@
+﻿namespace Desafio.Exception.ExceptionBase;
+
+public abstract class DesafioException : SystemException
+{
+    protected DesafioException(string message) : base(message)
+    {
+
+    }
+
+    public abstract int StatusCode { get; }
+    public abstract List<string> GetErrors();
+}
---
+++ DesafioFullStackPlenoBack/src/Desafio.Exception/ExceptionBase/InvalidLoginException.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+
+namespace Desafio.Exception.ExceptionBase;
+public class InvalidLoginException : DesafioException
+{
+    public InvalidLoginException() : base(ResourceErrorMessages.ERRO_EMAIL_OU_SENHA_INVALIDO)
+    {
+    }
+
+    public override int StatusCode => (int)HttpStatusCode.Unauthorized;
+
+    public override List<string> GetErrors()
+    {
+        return [Message];
+    }
+}
