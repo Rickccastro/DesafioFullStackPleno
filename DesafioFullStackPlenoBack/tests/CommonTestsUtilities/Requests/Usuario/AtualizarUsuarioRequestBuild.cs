7758713c423df9627318706286bf29a0723015da
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-08-21 21:39:50 -0300

    feat: criação do request para testes da entidade usuarios

---
+++ DesafioFullStackPlenoBack/tests/CommonTestsUtilities/Requests/Usuario/AtualizarUsuarioRequestBuild.cs
@@ -0,0 +1,17 @@
+﻿using Bogus;
+using Desafio.Communication.Requests.Usuario;
+
+namespace CommonTestsUtilities.Requests.Usuario;
+public class AtualizarUsuarioRequestBuild
+{
+    public static AtualizarUsuarioRequest Build()
+    {
+        return new Faker<AtualizarUsuarioRequest>()
+            .RuleFor(u => u.Id, _ => Guid.NewGuid())
+            .RuleFor(u => u.Nome, faker => faker.Person.FirstName)
+            .RuleFor(u => u.Email, (faker, user) => faker.Internet.Email(user.Nome))
+            .RuleFor(u => u.Senha, faker => faker.Internet.Password())
+            .RuleFor(u => u.Perfil, _ => "Usuario")
+            .Generate();
+    }
+}
---
+++ DesafioFullStackPlenoBack/tests/CommonTestsUtilities/Requests/Usuario/CriarUsuarioRequestBuild.cs
@@ -0,0 +1,16 @@
+﻿using Bogus;
+using Desafio.Communication.Requests.Usuario;
+
+namespace CommonTestsUtilities.Requests.Usuario;
+public class CriarUsuarioRequestBuild
+{
+    public static CriarUsuarioRequest Build()
+    {
+        return new Faker<CriarUsuarioRequest>()
+            .RuleFor(u => u.Nome, faker => faker.Person.FirstName)
+            .RuleFor(u => u.Email, (faker, user) => faker.Internet.Email(user.Nome))
+            .RuleFor(u => u.Senha, faker => faker.Internet.Password())
+            .RuleFor(u => u.Perfil, _ => "Usuario")
+            .Generate();
+    }
+}
