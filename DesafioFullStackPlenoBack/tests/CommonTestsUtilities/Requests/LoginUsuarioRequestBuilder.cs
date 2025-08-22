ef7772bd7258182dbd4f0a05cef6cc571126749e
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-08-21 21:40:21 -0300

    feat: criação do builder dos auxiliadores para testes

---
+++ DesafioFullStackPlenoBack/tests/CommonTestsUtilities/Requests/LoginUsuarioRequestBuilder.cs
@@ -0,0 +1,13 @@
+﻿using Bogus;
+using Desafio.Communication.Requests.Login;
+
+namespace CommonTestsUtilities.Requests;
+public class LoginUsuarioRequestBuilder
+{
+    public static LoginUsuarioRequest Build()
+    {
+        return new Faker<LoginUsuarioRequest>()
+                        .RuleFor(u => u.Email, (faker, user) => faker.Internet.Email())
+                        .RuleFor(u => u.Senha, faker => faker.Internet.Password());
+    }
+}
---
+++ DesafioFullStackPlenoBack/tests/CommonTestsUtilities/Token/JwtTokenGeneratorBuilder.cs
@@ -0,0 +1,16 @@
+﻿using Desafio.Application.Security;
+using Desafio.Domain.Entities;
+using Moq;
+
+namespace CommonTestsUtilities.Token;
+public class JwtTokenGeneratorBuilder
+{
+    public static IAccessTokenGenerator Build()
+    {
+        var mock = new Mock<IAccessTokenGenerator>();
+
+        mock.Setup(accessTokenGenerator => accessTokenGenerator.Generate(It.IsAny<Usuario>())).Returns("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c");
+
+        return mock.Object;
+    }
+}
