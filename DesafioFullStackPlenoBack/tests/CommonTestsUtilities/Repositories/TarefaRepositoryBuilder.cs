f6fa0b1ba6e679eba0c4b10c845a10dd04c3bec1
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-08-21 21:38:22 -0300

    feat: criação dos repositorios das entidades para testes

---
+++ DesafioFullStackPlenoBack/tests/CommonTestsUtilities/Repositories/TarefaRepositoryBuilder.cs
@@ -0,0 +1,13 @@
+﻿using Desafio.Domain.Repositories.Especificas;
+using Moq;
+
+namespace CommonTestsUtilities.Repositories;
+public class TarefaRepositoryBuilder
+{
+    public static ITarefaRepository Build()
+    {
+        var mock = new Mock<ITarefaRepository>();
+
+        return mock.Object;
+    }
+}
---
+++ DesafioFullStackPlenoBack/tests/CommonTestsUtilities/Repositories/UsuarioRepositoryBuilder.cs
@@ -0,0 +1,13 @@
+﻿using Desafio.Domain.Repositories.Especificas;
+using Moq;
+
+namespace CommonTestsUtilities.Repositories;
+public class UsuarioRepositoryBuilder
+{
+    public static IUsuarioRepository Build()
+    {
+        var mock = new Mock<IUsuarioRepository>();
+
+        return mock.Object;
+    }
+}
