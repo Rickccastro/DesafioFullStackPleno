f66711f65199ebc6fc3ae2a7f76d9423eed3fe59
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-08-21 21:37:24 -0300

    feat: criação do builder das entidades para testes

---
+++ DesafioFullStackPlenoBack/tests/CommonTestsUtilities/Entities/TarefaBuilder.cs
@@ -0,0 +1,19 @@
+﻿using Bogus;
+using Desafio.Domain.Entities;
+using Desafio.Domain.Enums;
+
+namespace CommonTestsUtilities.Entities;
+public static class TarefaBuilder
+{
+    public static Tarefa Build(Usuario usuario,StatusTarefa status = StatusTarefa.Pendente)
+    {
+        return new Faker<Tarefa>()
+        .RuleFor(t => t.Id, _ => Guid.NewGuid())
+        .RuleFor(t => t.Titulo, f => f.Lorem.Sentence(10)) 
+        .RuleFor(t => t.Descricao, f => f.Lorem.Paragraph(30))
+        .RuleFor(t => t.DataCriacao, f => DateTime.UtcNow)
+        .RuleFor(t => t.UsuarioId, _ => usuario.Id)
+        .RuleFor(t => t.Status, _ => status)
+        .Generate();
+    }
+}
---
+++ DesafioFullStackPlenoBack/tests/CommonTestsUtilities/Entities/UsuarioBuilder.cs
@@ -0,0 +1,19 @@
+﻿using Bogus;
+using Desafio.Domain.Entities;
+using Desafio.Domain.Enums;
+
+namespace CommonTestsUtilities.Entities;
+public class UsuarioBuilder
+{
+    public static Usuario Build(Perfis perfil = Perfis.Usuario)
+    {
+        return new Faker<Usuario>()
+            .RuleFor(u => u.Id, _ => Guid.NewGuid())
+            .RuleFor(u => u.Nome, faker => faker.Person.FirstName)
+            .RuleFor(u => u.Email, (faker, user) => faker.Internet.Email(user.Nome))
+            .RuleFor(u => u.Senha, faker => faker.Internet.Password())
+            .RuleFor(u => u.Perfil, _ => perfil)
+            .Generate();
+    }
+
+}
