ff460c59857055fa13a3a5462860ddd15047d708
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-08-23 10:32:06 -0300

    feat: metodo de build de lista de tarefas

--- DesafioFullStackPlenoBack/tests/CommonTestsUtilities/Entities/TarefaBuilder.cs
+++ DesafioFullStackPlenoBack/tests/CommonTestsUtilities/Entities/TarefaBuilder.cs
@@ -5,11 +5,24 @@ using Desafio.Domain.Enums;
 namespace CommonTestsUtilities.Entities;
 public static class TarefaBuilder
 {
+    public static List<Tarefa> BuildListTarefas(Usuario user, uint count = 5)
+    {
+        var list = new List<Tarefa>();
+
+        for (int i = 0; i < count; i++)
+        {
+            var tarefa = Build(user);
+
+            list.Add(tarefa);
+        }
+
+        return list;
+    }
     public static Tarefa Build(Usuario usuario,StatusTarefa status = StatusTarefa.Pendente)
     {
         return new Faker<Tarefa>()
         .RuleFor(t => t.Id, _ => Guid.NewGuid())
-        .RuleFor(t => t.Titulo, f => f.Lorem.Sentence(10)) 
+        .RuleFor(t => t.Titulo, f => f.Lorem.Sentence(10))
         .RuleFor(t => t.Descricao, f => f.Lorem.Paragraph(30))
         .RuleFor(t => t.DataCriacao, f => DateTime.UtcNow)
         .RuleFor(t => t.UsuarioId, _ => usuario.Id)
