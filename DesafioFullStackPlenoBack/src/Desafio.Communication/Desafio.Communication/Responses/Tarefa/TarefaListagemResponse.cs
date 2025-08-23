b3f317fa0c8a76aa00c885fccffdcd8affd309bb
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-08-23 10:49:15 -0300

    feat: criação do tarefa listagem response

---
+++ DesafioFullStackPlenoBack/src/Desafio.Communication/Desafio.Communication/Responses/Tarefa/TarefaListagemResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace Desafio.Communication.Responses.Tarefa;
+public class TarefaListagemResponse
+{
+    public string Titulo { get; set; } = null!;
+    public string Descricao { get; set; } = null!;
+}
