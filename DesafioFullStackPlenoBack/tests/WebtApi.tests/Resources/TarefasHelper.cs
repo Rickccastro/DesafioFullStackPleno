a75f0337fe6c4d28f0edb90affefa6d031b21e9e
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-08-23 10:36:20 -0300

    feat: criação de helper de tarefas e usuario

---
+++ DesafioFullStackPlenoBack/tests/WebtApi.tests/Resources/TarefasHelper.cs
@@ -0,0 +1,18 @@
+﻿using Desafio.Domain.Entities;
+using Desafio.Domain.Enums;
+
+namespace WebtApi.test.Resources;
+public class TarefasHelper
+{
+    public Tarefa _tarefa { get; set; }
+    public TarefasHelper(Tarefa tarefa)
+    {
+        _tarefa = tarefa;
+    }
+
+    public Guid GetId() => _tarefa.Id;
+    public string GetTitulo() => _tarefa.Titulo;
+    public string GetDescricao() => _tarefa.Descricao;
+    public DateTime GetDataCriada() => _tarefa.DataCriacao;
+    public StatusTarefa GetStatus() => _tarefa.Status;
+}
---
+++ DesafioFullStackPlenoBack/tests/WebtApi.tests/Resources/UsuarioHelper.cs
@@ -0,0 +1,19 @@
+﻿namespace WebtApi.test.Resources;
+public class UsuarioHelper
+{
+    private readonly Desafio.Domain.Entities.Usuario _usuario;
+    private readonly string _token;
+
+
+    public UsuarioHelper(Desafio.Domain.Entities.Usuario usuario, string token)
+    {
+        _usuario = usuario;
+        _token = token; 
+    }
+
+    public Guid GetId() => _usuario.Id;
+    public string GetNome() => _usuario.Nome;
+    public string GetEmail() => _usuario.Email;
+    public string GetSenha() => _usuario.Senha;
+    public string GetToken() => _token;
+}
