f434cb0d52c9b62dbf373a77a650a56a491b87a6
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-08-23 10:34:25 -0300

    refactor: utilizando string manual na descricao

--- DesafioFullStackPlenoBack/tests/CommonTestsUtilities/Requests/Tarefa/AtualizarTarefaRequestBuilder.cs
+++ DesafioFullStackPlenoBack/tests/CommonTestsUtilities/Requests/Tarefa/AtualizarTarefaRequestBuilder.cs
@@ -1,5 +1,8 @@
 ﻿using Bogus;
+using CommonTestsUtilities.Entities;
 using Desafio.Communication.Requests.Tarefa;
+using Desafio.Domain.Entities;
+
 
 namespace CommonTestsUtilities.Requests.Tarefa;
 public class AtualizarTarefaRequestBuilder
@@ -9,8 +12,8 @@ public class AtualizarTarefaRequestBuilder
         return new Faker<AtualizarTarefaRequest>()
         .RuleFor(t => t.Id, _ => Guid.NewGuid())
         .RuleFor(t => t.Titulo, f => f.Lorem.Sentence(10))
-        .RuleFor(t => t.Descricao, f => f.Lorem.Paragraph(30))
-        .RuleFor(t => t.DataCriacao, f => DateTime.UtcNow)
+        .RuleFor(t => t.Descricao, f => "olá")
+        .RuleFor(t => t.UsuarioId, f => UsuarioBuilder.Build().Id)
         .RuleFor(t => t.Status, _ => "Pendente")
         .Generate();
     }
