06e7d6c244efab7e8652d482a3727e97d39f5ad3
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-08-23 10:44:28 -0300

    feat: adição de filtro de exceptions para responses ao usuario

---
+++ DesafioFullStackPlenoBack/src/Desafio.Api/Filters/ExceptionFilter.cs
@@ -0,0 +1,39 @@
+﻿using Desafio.Communication.Responses.Error;
+using Desafio.Exceptions;
+using Desafio.Exceptions.ExceptionBase;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Desafio.Api.Filters;
+
+public class ExceptionFilter : IExceptionFilter
+{
+    public void OnException(ExceptionContext context)
+    {
+        if (context.Exception is DesafioException)
+        {
+            HandleProjectException(context);
+        }
+        else
+        {
+            ThrowUnkowError(context);
+        }
+    }
+
+    private void HandleProjectException(ExceptionContext context)
+    {
+        var cashFlowException = (DesafioException)context.Exception;
+        var errorResponse = new ResponseError(cashFlowException.GetErrors());
+
+        context.HttpContext.Response.StatusCode = cashFlowException.StatusCode;
+        context.Result = new ObjectResult(errorResponse);
+    }
+
+    private void ThrowUnkowError(ExceptionContext context)
+    {
+        var errorResponse = new ResponseError(ResourceErrorMessages.ERRO_DESCONHECIDO);
+
+        context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        context.Result = new ObjectResult(errorResponse);
+    }
+}
