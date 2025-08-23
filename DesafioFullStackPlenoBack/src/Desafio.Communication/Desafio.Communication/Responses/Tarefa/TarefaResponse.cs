230510815eab0682702d69cded24bd8100959939
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-08-23 10:49:57 -0300

    feat: adicionando campos no tarefa response

--- DesafioFullStackPlenoBack/src/Desafio.Communication/Desafio.Communication/Responses/Tarefa/TarefaResponse.cs
+++ DesafioFullStackPlenoBack/src/Desafio.Communication/Desafio.Communication/Responses/Tarefa/TarefaResponse.cs
@@ -1,6 +1,15 @@
 ﻿namespace Desafio.Communication.Responses.Tarefa;
 public class TarefaResponse
 {
+    public Guid Id { get; set; }
+
     public string Titulo { get; set; } = null!;
-    public string Descricao { get; set; } = null!;
+
+    public string? Descricao { get; set; }
+
+    public string Status { get; set; } = null!;
+
+    public DateTime DataCriacao { get; set; } = DateTime.UtcNow;
+
+    public Guid UsuarioId { get; set; }
 }
