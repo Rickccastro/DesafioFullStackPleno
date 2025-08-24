46f5785922f5b4255a80e24c0d6b40938a6d1955
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-08-23 21:35:55 -0300

    feat: adicionando rotas da aplicação

--- DesafioFullStackPlenoFront/src/app/app.routes.ts
+++ DesafioFullStackPlenoFront/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { UsuariosManagerComponent } from './features/usuarios/usuarios-manager/usuarios-manager.component';
+import { LoginUsuariosComponent } from './features/login/login-usuarios/login-usuarios.component';
+import { TarefasManagerComponent } from './features/tarefas/tarefas-manager/tarefas-manager.component';
+import { usuarioResolver } from './core/resolvers/usuario.resolver';
+import { tarefasResolver } from './core/resolvers/tarefas.resolver';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+ {
+  path:'login',
+  component: LoginUsuariosComponent,
+}, 
+ {
+  path:'usuarios',
+  component: UsuariosManagerComponent,
+},
+{
+  path:'tarefas',
+  component: TarefasManagerComponent,
+      resolve: {
+      usuarios: usuarioResolver,
+      tarefas: tarefasResolver
+  }
+} 
+];
