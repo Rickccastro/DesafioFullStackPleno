15236bc1abeae8d01bb1e6a5c6f5fbdd76d1132e
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-08-23 22:04:48 -0300

    feat: tela de usuarios completa

--- DesafioFullStackPlenoFront/src/app/features/usuarios/usuarios-manager/usuarios-manager.component.ts
+++ DesafioFullStackPlenoFront/src/app/features/usuarios/usuarios-manager/usuarios-manager.component.ts
@@ -14,8 +14,66 @@ import { CommonModule } from '@angular/common';
   styleUrl: './usuarios-manager.component.scss'
 })
 export class UsuariosManagerComponent implements OnInit {
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
+  usuarioService = inject(UsuarioService);
+  form!: FormGroup;
+  editingUsuario: Usuario | null = null;
+  perfilOptions: string[] = [];
+  listUsuarios = signal<Usuario[]>([]);
+  private route = inject(ActivatedRoute);
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      nome: new FormControl('', [Validators.required]),
+      email: new FormControl(''),
+      perfil: new FormControl('', [Validators.required]),
+      senha: new FormControl('', [Validators.required]),
+    });
+
+    this.route.data.subscribe((data) => {
+      this.listUsuarios.set(data['usuarios']);
+    });
+
+    this.perfilOptions = ['Usuario', 'Administrador'];
   }
-  
+
+submit() {
+  if (this.form.invalid) return;
+
+  if (this.editingUsuario) {
+    const usuarioAtualizado = {
+      ...this.editingUsuario,
+      ...this.form.value,
+    };
+
+    this.usuarioService.atualizar(usuarioAtualizado).subscribe(() => {
+      this.listUsuarios.update((usuarios) =>
+        usuarios.map((u) =>
+          u.id === usuarioAtualizado.id ? usuarioAtualizado : u
+        )
+      );
+      this.editingUsuario = null;
+    });
+
+  } else {
+    this.usuarioService.adicionar(this.form.value).subscribe((novoUsuario) => {
+      this.listUsuarios.update((usuarios) => [...usuarios, novoUsuario]);
+    });
+  }
+    this.form.reset();
+}
+  edit(usuario: Usuario) {
+    this.editingUsuario = usuario;
+
+    this.form.patchValue({
+      nome: usuario.nome,
+      email: usuario.email,
+      perfil: usuario.perfil,
+      senha: usuario.senha,
+    });
+  }
+
+  cancelEdit() {
+    this.editingUsuario = null;
+    this.form.reset();
+  }  
 }
