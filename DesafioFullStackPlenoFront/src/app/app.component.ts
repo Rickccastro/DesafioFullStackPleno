49aac45ccb7c81be0d67de3fcaf4a02a6c235b43
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-08-23 21:35:35 -0300

    remove: removendo trecho de codigo desnecessario

--- DesafioFullStackPlenoFront/src/app/app.component.ts
+++ DesafioFullStackPlenoFront/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UsuariosManagerComponent } from './features/usuarios/usuarios-manager/usuarios-manager.component';
+import { LoginUsuariosComponent } from './features/login/login-usuarios/login-usuarios.component';
 
 @Component({
   selector: 'app-root',
