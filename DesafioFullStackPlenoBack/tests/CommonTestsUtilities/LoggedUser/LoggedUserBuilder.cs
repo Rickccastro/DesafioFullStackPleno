468bf520e768eda0820a1797cb8aeda98e56eed2
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-08-24 21:12:46 -0300

    Revert "remove: classe desnecessaria"
    This reverts commit 377496454d3e66d159d3163725fd225fa203ae24.

---
+++ DesafioFullStackPlenoBack/tests/CommonTestsUtilities/LoggedUser/LoggedUserBuilder.cs
@@ -0,0 +1,16 @@
+﻿using Desafio.Application.Security;
+using Desafio.Domain.Entities;
+using Moq;
+
+namespace CommonTestsUtilities.LoggedUser;
+public class LoggedUserBuilder
+{
+    public static ILoggedUser Build(Usuario user)
+    {
+        var mock = new Mock<ILoggedUser>();
+
+        mock.Setup(loggedUser => loggedUser.Get()).ReturnsAsync(user);
+
+        return mock.Object;
+    }
+}
