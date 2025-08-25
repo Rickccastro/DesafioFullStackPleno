b984a12f972a6c8a944e00b5ceedbb666df41900
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-08-24 21:06:11 -0300

    feat: adicionando camada de layout no app component

--- DesafioFullStackPlenoFront/src/app/app.component.ts
+++ DesafioFullStackPlenoFront/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UsuariosManagerComponent } from './features/usuarios/usuarios-manager/usuarios-manager.component';
 import { LoginUsuariosComponent } from './features/login/login-usuarios/login-usuarios.component';
+import { LayoutComponent } from "./core/layouts/layout/layout.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
