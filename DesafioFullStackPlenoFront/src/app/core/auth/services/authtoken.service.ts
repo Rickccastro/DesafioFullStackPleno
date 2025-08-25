84dc076c3d3bcbb3b48673a84d66aea5449f7a16
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-08-24 21:08:34 -0300

    feat: metodo de pegar o perfil do usuario

--- DesafioFullStackPlenoFront/src/app/core/auth/services/authtoken.service.ts
+++ DesafioFullStackPlenoFront/src/app/core/auth/services/authtoken.service.ts
@@ -1,11 +1,15 @@
-import { Injectable } from '@angular/core';
-import {jwtDecode} from 'jwt-decode';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { jwtDecode } from 'jwt-decode';
+import { Observable } from 'rxjs';
+import { LoginResponse } from '../../models/Responses/LoginResponse';
+import { RefreshResponse } from '../../models/Responses/RefreshResponse';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthTokenService {
-private readonly TOKEN_KEY = 'auth_token'; 
+  private readonly TOKEN_KEY = 'auth_token';
 
   setToken(token: string): void {
     localStorage.setItem(this.TOKEN_KEY, token);
@@ -22,15 +26,13 @@ private readonly TOKEN_KEY = 'auth_token';
   isLoggedIn(): boolean {
     return !!this.getToken();
   }
-  getUserRole():string {
-    const token  = this.getToken();
-    if(!token){
-      return ''
+  getUserRole(): string {
+    const token = this.getToken();
+    if (!token) {
+      return '';
     }
 
     const decoded: any = jwtDecode(token);
-    return decoded.role || null; 
+    return decoded.role || null;
   }
-
 }
-
