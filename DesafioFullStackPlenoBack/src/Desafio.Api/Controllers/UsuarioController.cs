9e04c7b4445f9d4a07b9cf9f1c38a7567661a9d4
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-08-24 21:14:37 -0300

    fix: correção de rotas com autorização no perfil

--- DesafioFullStackPlenoBack/src/Desafio.Api/Controllers/UsuarioController.cs
+++ DesafioFullStackPlenoBack/src/Desafio.Api/Controllers/UsuarioController.cs
@@ -1,5 +1,6 @@
 ﻿using Desafio.Application.UseCase.Usuario.Atualizar;
 using Desafio.Application.UseCase.Usuario.Cria;
+using Desafio.Application.UseCase.Usuario.Listar;
 using Desafio.Communication.Requests.Usuario;
 using Desafio.Communication.Responses.Error;
 using Desafio.Communication.Responses.Tarefa;
@@ -10,7 +11,6 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Desafio.Api.Controllers;
 [Route("[controller]")]
-[Authorize(Roles = nameof(Perfis.Administrador))]
 [ProducesResponseType(typeof(ResponseError), StatusCodes.Status401Unauthorized)]
 [ProducesResponseType(typeof(ResponseError), StatusCodes.Status403Forbidden)]
 [ApiController]
@@ -18,6 +18,7 @@ public class UsuarioController : ControllerBase
 {
     [HttpPost]
     [ProducesResponseType(typeof(UsuarioResponse), StatusCodes.Status201Created)]
+    [Authorize(Roles = nameof(Perfis.Administrador))]
     [ProducesResponseType(typeof(ResponseError), StatusCodes.Status400BadRequest)]
     public async Task<ActionResult> CriarUsuario([FromServices] ICriarUsuarioUseCase useCase, [FromBody] CriarUsuarioRequest request)
     {
@@ -25,10 +26,21 @@ public class UsuarioController : ControllerBase
 
         return Created(string.Empty,nomeUsuario);
     }
+    [HttpGet]
+    [ProducesResponseType(typeof(UsuarioResponse), StatusCodes.Status200OK)]
+    [Authorize(Roles = nameof(Perfis.Administrador) + "," + nameof(Perfis.Usuario))]
+    [ProducesResponseType(typeof(ResponseError), StatusCodes.Status404NotFound)]
+    public async Task<ActionResult> GetUsuarios([FromServices] IListaUsuariosUseCase useCase)
+    {
+        var listaUsuarios = await useCase.ListarUsuarios();
+
+        return Ok(listaUsuarios);
+    }
 
     [Authorize]
     [HttpPut]
     [ProducesResponseType(typeof(UsuarioResponse), StatusCodes.Status201Created)]
+    [Authorize(Roles = nameof(Perfis.Administrador))]
     [ProducesResponseType(typeof(ResponseError), StatusCodes.Status400BadRequest)]
     public async Task<ActionResult> AtualizarUsuario([FromServices] IAtualizarUsuarioUseCase useCase, [FromBody] AtualizarUsuarioRequest request)
     {
